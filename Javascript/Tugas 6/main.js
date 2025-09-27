2473938bcee9d52f7fc80b916e1756c016001ea8
Dwimutiaafifah04 <afifahdwimutia04@gmail.com>
2025-09-27 17:05:07 +0700

    uploud tugas

--- Javascript/Tugas 6/main.js
+++ Javascript/Tugas 6/main.js
@@ -1,21 +1,21 @@
-// main.js
+// main.js //
 import { index, store, destroy } from "./controller.js";
 
 const main = () => {
     console.log('=== Daftar User Awal ===');
-    index(); // Tampilkan data awal
+    index(); // Menampilkan data awal //
 
     console.log('\n=== Menambah Data ===');
-    store(); // Tambah dua data
+    store(); // Menambahkan dua data //
 
     console.log('\n=== Daftar User Setelah Ditambah ===');
-    index(); // Tampilkan data setelah ditambah
+    index(); // Menampilkan data setelah dilakukannya tambah data //
 
     console.log('\n=== Menghapus Data ===');
-    destroy(); // Hapus data terakhir
+    destroy(); // Menghapus data terakhir //
 
     console.log('\n=== Daftar User Setelah Dihapus ===');
-    index(); // Tampilkan data setelah dihapus
+    index(); // Menampilkan data setelah dilakukannya penghapusan
 }
 
 main();
